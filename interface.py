83902d612f48e97aaba41acb1ed8f0be5dd8aae7
Kayzer742 <152960097+Kayzer742@users.noreply.github.com>
2026-01-12 14:12:11 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -9,35 +9,9 @@ import streamlit as st
 import hmac
 import streamlit as st
 
-
-def check_password():
-    """Returns `True` if the user had the correct password."""
-
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
-            st.session_state["password_correct"] = True
-        else:
-            st.session_state["password_correct"] = False
-
-    # Return True if the passward is validated.
-    if st.session_state.get("password_correct", False):
-        return True
-
-    # Show input for password.
-    st.text_input(
-        "Password", type="password", on_change=password_entered, key="password"
-    )
-    if "password_correct" in st.session_state:
-        st.error("😕 Password incorrect")
-    return False
-    
-if not check_password():
-    st.stop()  # Do not continue if check_password is not True.
-
 def runstreamlit () :
     
-    st.title("Bienvenue sur Dimoclim")
+    st.title("Interface 1.0")
 
     option = st.selectbox('Localisation ? ',
     ('EU',
